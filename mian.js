5d90be0cf2f7ae40bd7ebe39426a5a52b22a0671
Pudtipong05 <151870664+Pudtipong05@users.noreply.github.com>
2024-01-20 12:18:04 +0700

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,11 @@
+const loginForm = document.querySelector(".login-form");
+e.preventDefault();
+console.input_username = document.querySelector("#username");
+console.input_password = document.querySelector("#password");
+
+const hdILogin = (e) => {
+  e.preventDefault();
+  console.log(loginForm.elements);
+  let;
+};
+loginForm.addEventListener("submit", hdILogin);
